46a97019747a49bd5b6a2201a198aea202f6302f
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-08-02 00:58:41 +0600

    Completed the navbar

---
+++ app.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#12141D",
+        main: "#FF664F",
+      },
+    },
+    fontSize: {
+      base: "13px",
+      normal: "16px",
+    },
+    fontFamily: {
+      inter: "'Inter', sans- serif",
+    },
+  },
+};
