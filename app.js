66ecd3aaac45daa07b2f6dac15d44e6c390b00d6
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-08-02 11:50:54 +0600

    Completed the hero section

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ tailwind.config = {
       colors: {
         primary: "#12141D",
         main: "#FF664F",
+        para: "rgba(135, 136, 141, 1)",
       },
     },
     fontSize: {
+      sm: "12px",
       base: "13px",
       normal: "16px",
+      large: "42px",
+      little: "26px",
     },
     fontFamily: {
       inter: "'Inter', sans- serif",
+    },
+    lineHeight: {
+      large: "46px",
+      little: "32px",
+      sm: "18px",
     },
   },
 };
